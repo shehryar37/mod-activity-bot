9e7864592e8c41530c9f004f7e2121d6873f908d
shehryar37 <sasubhani7@outlook.com>
2020-08-11 23:04:02 +0500

    Added discord compatibility

--- __init__.py
+++ __init__.py
@@ -1,19 +1,33 @@
 import praw
 import os
 
+import discord
+from discord.ext import commands
+import asyncio
+
 
 def main():
-    reddit = login()
+    # Tries getting access to the Reddit bot
+    reddit_client = reddit_access()
 
-    # Makes a Redditor object out of all the mods
-    subreddit = reddit.subreddit('friends')
+    # Tries getting access to the Discord bot
+    discord_client = discord_access()
 
+    # All the mods have been added as friends on the account.
+    # So it only has to access r/friends
+    subreddit = reddit_client.subreddit('friends')
+
+    # Loops over every new comment in r/friends
     for comment in subreddit.stream.comments(skip_existing=True):
-        print("{} has just commented in {}: {}".format(
-            comment.author.name, comment.subreddit_name_prefixed, comment.link_permalink + comment.id))
+        message = "{} has just commented in {}: {}".format(
+            comment.author.name, comment.subreddit_name_prefixed, comment.link_permalink + comment.id)
+
+    # Gets the channel ID
+    channel = discord_client.get_channel(742799267294609448)
+    channel.send(message)
 
 
-def login():
+def reddit_access():
     try:
         reddit = praw.Reddit(username=os.environ["USERNAME"],
                              password=os.environ["PASSWORD"],
@@ -22,11 +36,29 @@ def login():
                              user_agent=os.environ["USER_AGENT"]
                              )
 
-        print("Logged in")
+        print("Accessed Reddit")
 
         return reddit
+
     except Exception as e:
         print(e)
+        exit(self, 1)
+
+
+def discord_access():
+    discord_client = commands.Bot(command_prefix='.')
+
+    try:
+        @discord_client.event
+        async def on_ready():
+            print("Accessed Discord")
+
+        discord_client.run(os.environ["CLIENT_TOKEN"])
+
+        return discord_client
+    except Exception as e:
+        print(e)
+        exit(self, 2)
 
 
 main()
